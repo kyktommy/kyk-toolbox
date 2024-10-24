5aec4ed3932a5430eac99cbfed4e577885d08b79
tommykwanpgpg <tommy.kwan@pgpg.la>
2024-10-24 15:05:42 +0800

    chore: github action gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/kyk-toolbox',
+}
 
-export default nextConfig;
+export default nextConfig
